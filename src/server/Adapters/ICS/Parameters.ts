50ae61062c9c6f8950aa7013d96f8be7473bba26
David <filecage@users.noreply.github.com>
2023-07-04 17:06:03 +0200

    adds all Parameters with values of RFC5545

---
+++ src/server/Adapters/ICS/Parameters.ts
@@ -0,0 +1,33 @@
+
+export namespace Parameters {
+    // TODO: These types could be replaced with something that actually helps accessing the data
+    type Uri = string;
+    type CalAddress = string;
+    type LanguageTag = string;
+
+    // @see https://datatracker.ietf.org/doc/html/rfc5545#section-3.2
+    export type AlternateTextRepresentation = { ALTREP?: CalAddress };
+    export type CommonName = { CN?: string };
+    export type CalendarUserType = { CUTYPE?: string | 'INDIVIDUAL' | 'GROUP' | 'RESOURCE' | 'ROOM' | 'UNKNOWN' };
+    export type Delegators = { 'DELEGATED-FROM'?: CalAddress[] };
+    export type Delegatees = { 'DELEGATED-TO'?: CalAddress[] };
+    export type DirectoryEntryReference = { DIR?: Uri };
+    export type InlineEncoding = { ENCODING?: '8BIT' | 'BASE64' };
+    export type FormatType = { FMTTYPE?: string };
+    export type FreeBusyTimeType = { FBTYPE?: string | 'FREE' | 'BUSY' | 'BUSY-UNAVAILABLE' | 'BUSY-TENTATIVE' };
+    export type Language = { LANGUAGE?: LanguageTag };
+    export type GroupOrListMembership = { MEMBER?: CalAddress[] };
+    export type ParticipationStatusJournal = { PARTSTAT?: string | 'NEEDS-ACTION' | 'ACCEPTED' | 'DECLINED' };
+    export type ParticipationStatusEvent = { PARTSTAT?: ParticipationStatusJournal['PARTSTAT'] | 'TENTATIVE' | 'DELEGATED' };
+    export type ParticipationStatusTodo = { PARTSTAT?: ParticipationStatusEvent['PARTSTAT'] | 'COMPLETED' | 'IN-PROCESS' };
+    export type Range = { RANGE?: 'THISANDFUTURE' };
+    export type AlarmTriggerRelationship = { RELATED?: 'START' | 'END' };
+
+    // /** @default PARENT */
+    export type RelationshipType = { RELTYPE?: string | 'PARENT' | 'CHILD' | 'SIBLING' };
+    export type ParticipationRole = { ROLE?: string | 'CHAIR' | 'REQ-PARTICIPANT' | 'OPT-PARTICIPANT' | 'NON-PARTICIPANT'};
+    export type RSVPExpectation = {RSVP?: 'TRUE' | 'FALSE'};
+    export type SentBy = {'SENT-BY'?: CalAddress};
+    export type TimeZoneIdentifier = {TZID?: string};
+    export type ValueDataTypes = {VALUE?: string | 'BINARY' | 'BOOLEAN' | 'CAL-ADDRESS' | 'DATE' | 'DATE-TIME' | 'DURATION' | 'FLOAT' | 'INTEGER' | 'PERIOD' | 'RECUR' | 'TEXT' | 'TIME' | 'URI' | 'UTC-OFFSET'};
+}
