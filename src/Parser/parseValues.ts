e673b904efd7970ba8687bf523b32f145afa3905
David <filecage@users.noreply.github.com>
2023-07-18 13:44:56 +0200

    correctly parse quoted content value lines

--- src/Parser/parseValues.ts
+++ src/Parser/parseValues.ts
@@ -1,19 +1,34 @@
-import {COMMA} from "./Constants";
+import {COMMA, QUOTES} from "./Constants";
 import {DateTime} from "./ValueTypes/DateTime";
 import {Period} from "./ValueTypes/Period";
 import {Parameters} from "./Parameters/Parameters";
+
+const matchDoubleQuotesString = `${QUOTES}([^${QUOTES}\\\\]*(\\\\.[^${QUOTES}\\\\]*)*)${QUOTES}`;
+const escapedDoubleQuotesStringRegex = new RegExp(`^${matchDoubleQuotesString}$`);
+const listStringRegex = new RegExp(`((${matchDoubleQuotesString})|([^${COMMA}]+))(${COMMA}|$)`, 'g');
 
 export interface ValueParserFn<T extends {} = {}> {
     (value: string, parameters: T): string|string[]|Period|DateTime|number
 }
 
-export function parseList (value: string) : string[] {
-    // TODO: Implement correct handling for values in quotes
-    return value.split(COMMA);
+export function parseList (input: string) : string[] {
+    const values = [];
+    const matches = input.matchAll(listStringRegex);
+    for (const match of matches) {
+        const value = match[3] || match[1];
+        values.push(value.replaceAll('\\"', '"'));
+    }
+
+    return values;
 }
 
 export function parseValueRaw (value: string) : string {
-    return value;
+    const enquoted = value.match(escapedDoubleQuotesStringRegex);
+    if (!enquoted || enquoted.length === 0) {
+        return value;
+    }
+
+    return enquoted[1].replaceAll('\\"', '"');
 }
 
 export function parseNumber (value: string) : number {
