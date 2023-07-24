c38ef2e799ce61afc30bbc1912e58b58bbc210f6
David <filecage@users.noreply.github.com>
2023-07-24 17:06:09 +0200

    remove production dependency to ts-xor

---
+++ src/Types/XOR.ts
@@ -0,0 +1,42 @@
+/* eslint-disable */
+
+/**
+ * Imported from https://github.com/maninak/ts-xor/tree/master to avoid dependency
+ * Original license & copyright:
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2019 Konstantinos Maninakis maninak@protonmail.com
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+/**
+ * Resolve mapped types and show the derived keys and their types when hovering in
+ * VS Code, instead of just showing the names those mapped types are defined with.
+ */
+type Prettify<T> = {
+    [K in keyof T]: T[K]
+} & {}
+
+/**
+ * Get the keys of T without any keys of U.
+ */
+type Without<T, U> = {
+    [P in Exclude<keyof T, keyof U>]?: never
+}
+
+/**
+ * Restrict using either exclusively the keys of T or exclusively the keys of U.
+ *
+ * No unique keys of T can be used simultaneously with any unique keys of U.
+ *
+ * Example:
+ * `const myVar: XOR<T, U>`
+ *
+ * More: https://github.com/maninak/ts-xor/tree/master#description
+ */
+export type XOR<T, U> = (T | U) extends object
+    ? (Prettify<Without<T, U> & U>) | (Prettify<Without<U, T> & T>)
+    : T | U
